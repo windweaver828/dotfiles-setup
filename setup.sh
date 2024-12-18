f3ca934a1b19a9be4713fd4356a2f6928df68237
Keith Brandenburg <windweaver828@gmail.com>
2024-12-18 12:31:04 -0600

    Working on new setup.sh script

--- setup.sh
+++ setup.sh
@@ -2,15 +2,83 @@
 
 # Delete all the dotfiles
 cd $HOME
-rm -rf .dot .dotfiles .zsh_history .zshrc .zcompdump-localhost-5.9 .zcompdump-localhost-5.9.zwc .tmux .tmux.conf .oh-my-zsh .pylintrc .gitignore .git-credentials .gitconfig .gitmodules .config/lsd .config/nvim .local/share/nvim
+rm -rf .dot .dotfiles .zsh_history .zshrc .zcompdump* .tmux .tmux.conf .oh-my-zsh .pylintrc .gitignore .git-credentials .gitconfig .gitmodules .config/lsd .config/nvim .local/share/nvim
+
+if [[ ${1} == "clean" ]]; then
+    echo "Cleaning up dev environment"
+    SCRIPT_DIR=$(dirname "$(realpath "$0")")
+    if [ -d /mnt/c/Users/Keith ]; then
+        echo "Showershop detected, removing extra files"
+        cd "$HOME"
+        rm -rf .viminfo* .zcompdump* .zsh_history projects/.git showershop/.git brandisgarage
+        cd /mnt/c/Users/Keith/
+        rm -rf .gitconfig .git-credentials passwords.json Documents/Projects/passwords.json
+    fi
+    rm -rf "$SCRIPT_DIR"
+    echo "Cleanup complete"
+    exit
+fi
+
+echo "Setting up dev environment"
 
 if [[ ! -e $HOME/.dot ]]; then
-    git clone https://windweaver828@bitbucket.org/windweaver828/dotfiles.git $HOME/.dot
-    /usr/bin/git --git-dir=$HOME/.dot/.git/ --work-tree=$HOME restore .
-    /usr/bin/git --git-dir=$HOME/.dot/.git/ --work-tree=$HOME submodule update --init --recursive --force
-    /usr/bin/git --git-dir=$HOME/.dot/.git/ --work-tree=$HOME config --local status.showUntrackedFiles no
+    # >/dev/null 2>&1
+    git clone --no-checkout https://windweaver828@bitbucket.org/windweaver828/dotfiles.git $HOME/.dot
+    alias dot='/usr/bin/git --git-dir=$HOME/.dot/.git/ --work-tree=$HOME'
+    dot restore .
+    dot submodule update --init --recursive --force
+    dot config --local status.showUntrackedFiles no
 fi
 
 # Install oh-my-zsh and custom files
 rm -rf $HOME/.oh-my-zsh/custom
 ln -s $HOME/.dotfiles/oh-my-zsh-custom $HOME/.oh-my-zsh/custom
+
+# List of packages needed to be installed
+dependencies=("curl zsh tmux git lsd bat fonts-powerline ncurses-term neovim")
+IFS=' ' read -ra dependencies <<< $dependencies # convert to array
+need_install=""
+# If apt available, install dependencies
+if [[ -n $(which apt) ]]; then
+    echo "Found apt - checking and installing recommended packages"
+    for package in "${dependencies[@]}"; do
+        if ! dpkg -l | grep -q "^ii  $package "; then
+            need_install+="$package ";
+        fi
+    done
+    if [[ -n ${need_install} ]]; then
+        echo "Installing packages: ${need_install}"
+        sudo apt update >/dev/null 2>&1
+        sudo apt install -y ${need_install} >/dev/null 2>&1
+    else
+        echo "No packages missing"
+    fi
+    if [[ -z $(which lsd) ]]; then
+        echo "Failed to install lsd, install manually"
+        echo "https://github.com/lsd-rs/lsd/releases"
+    fi
+else
+    echo "System does not use apt, you will need to ensure packages are installed manually"
+    echo ${dependencies}
+fi
+
+# Install FiraCode Fonts
+if [[ -n $(which fc-cache) ]]; then
+    if [[ -z $(fc-list | grep -i "firacode") ]]; then
+        if [ -d /usr/local/share/fonts ]; then
+            echo "Installing FiraCode NerdFonts"
+            cp $HOME/.dotfiles/FiraCodeNerdFont/*.ttf /usr/local/share/fonts/
+            fc-cache -fv >/dev/null 2>&1
+        else
+            echo "/usr/local/share/fonts not found, you will need to install the FiraCodeNerdFont fonts manually"
+        fi
+    fi
+    echo "fc-cache not found, you will need to install the FiraCodeNerdFont fonts manually"
+fi
+
+# Open and close nvim to install lazy and all plugins
+nvim --headless -c 'qa' >/dev/null 2>&1
+
+
+source $HOME/.zshrc
+echo "Installation Complete"
